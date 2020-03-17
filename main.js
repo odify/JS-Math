4effba3d4d5f2811a7b1e64131f8459aa18fd8a1
odify <odify495@gmail.com>
2020-03-17 15:41:39 +0100

    V0.0.1

---
+++ main.js
@@ -0,0 +1,94 @@
+//No.1
+
+// console.log(Math.PI)
+
+// const roundedPI = 3.1415;
+
+// console.log(round(Math.PI))
+
+//No.2
+
+// toRound = (x) => {
+//     return Math.round(x);
+// }
+
+// console.log(toRound(PI))
+// console.log(toRound(9.45))
+// console.log(toRound(193.4464))
+// console.log(toRound(0.8596433607))
+// console.log(toRound(0.65149992))
+// console.log(toRound(2.1807301500))
+// console.log(toRound(- 2.9406290890))
+// console.log(toRound(- 24.45))
+
+// toRound = (x) => {
+//     let y = Math.round(x);
+//     return y
+// }
+// let y = 9.45;
+// toRound(y)
+
+// console.log(toRound(y))
+
+//No.3
+
+// let randomNum = Math.random();
+// let randomNum1_10 = Math.floor(Math.random() * 10) + 1;
+// let randomNum1_100 = Math.floor(Math.random() * 100) + 1;
+
+
+// console.log(randomNum)
+// console.log(randomNum1_10)
+// console.log(randomNum1_100)
+
+//No.4
+
+// let min = Math.min(393, 205, 479, 47, 376, 267, 385, 2, 190, 383, 286, 462, 115, 138, 331, 409, 427, 245, 224, 276, 483, 55, 147, 177, 208, 347, 135, 282, 33, 270, 475, 426, 476, 453, 474, 351, 219, 358, 354, 410, 240, 139, 371, 356, 277, 202, 264, 204, 447, 488);
+// console.log(min)
+
+// let max = Math.max(393, 205, 479, 47, 376, 267, 385, 2, 190, 383, 286, 462, 115, 138, 331, 409, 427, 245, 224, 276, 483, 55, 147, 177, 208, 347, 135, 282, 33, 270, 475, 426, 476, 453, 474, 351, 219, 358, 354, 410, 240, 139, 371, 356, 277, 202, 264, 204, 447, 488);
+// console.log(max)
+
+//No.5
+
+
+// let zahlenRunden = [PI, 9.45, 193.4464, 0.8596433607, 0.65149992,
+//     2.1807301500, - 2.9406290890, - 24.45]
+
+// zahlenRunden.forEach(elt => console.log(Math.round(elt)))
+
+
+//No.6
+
+// let userChoice = prompt("Schätze eine Zahl zwischen 1 bis 10");
+// let randomNum1_10 = Math.floor(Math.random() * 10) + 1;
+
+// console.log("Random-Number is " + randomNum1_10)
+// console.log("UserChoice is " + userChoice)
+
+// if (userChoice == randomNum1_10) {
+//     console.log(`Du hast richtig geraten. Die Zahl lautet ${randomNum1_10}`)
+// } else {
+//     console.log(`Du hast falsch geraten. Die Zahl lautet ${randomNum1_10}`)
+// }
+
+
+//No.7
+
+// function minimalWerte() {
+
+//     let dasAlterDerEltern = Math.min(65, 59);
+
+//     let dasAlterDerGeschwister = Math.min(12, 20, 6);
+//     let dasAlterDerTrainer = Math.min(40, 28, 30, 50);
+//     let dasAlterDerFussballspieler = Math.min(35, 55, 66, 22, 33, 21, 32);
+//     console.log(
+//         "Der Jüngste in der Gruppe ist:",
+//        
+//   );
+// }
+
+// minimalWerte();
+
+
+
